41b5166b6a767744c895b21f78b1665133408dc8
yuedong111 <hchenpeng@163.com>
2019-08-19 15:54:40 +0800

    normal

--- meituan.py
+++ meituan.py
@@ -12,23 +12,51 @@ import random
 import sys
 from jiehun_meishi import parse_jiehun_item, parse_meishi_item
 from meituanpeixun import parse_peixun
+from functools import wraps
+
+
+def second_run(func):
+    count = 0
+
+    @wraps(func)
+    def decorate(*args, **kwargs):
+        nonlocal count
+        try:
+            res = func(*args, **kwargs)
+        except Exception as e:
+            print(traceback.print_exc())
+            while True:
+                time.sleep(2)
+                print("run again {} {}".format(count, args))
+                count = count + 1
+                if count >= 5:
+                    count = 0
+                    return {}
+                res = decorate(*args, **kwargs)
+                break
+        count = 0
+        return res
+
+    return decorate
+
 
 url_map = {
     # "life": "https://cq.meituan.com/shenghuo/pn{}/",
     # "xiuxian": "https://cq.meituan.com/xiuxianyule/pn{}/",
     # "meishi": "https://cq.meituan.com/meishi/pn{}/",
-    "jiankangliren": "https://cq.meituan.com/jiankangliren/pn{}/",
-    "jiehun": "https://cq.meituan.com/jiehun/pn{}/",
-    "qinzi": "https://cq.meituan.com/qinzi/pn{}/",
-    "yundong": "https://cq.meituan.com/yundongjianshen/pn{}/",
-    "jiazhuang": "https://cq.meituan.com/jiazhuang/pn{}/",
-    "yiliao": "https://cq.meituan.com/yiliao/pn{}/",
-    "jiaoyupeixun": "https://cq.meituan.com/xuexipeixun/pn{}/"
+    # "jiankangliren": "https://cq.meituan.com/jiankangliren/pn{}/",
+    # "jiehun": "https://cq.meituan.com/jiehun/pn{}/",
+    # "qinzi": "https://cq.meituan.com/qinzi/pn{}/",
+    # "yundong": "https://cq.meituan.com/yundongjianshen/pn{}/",
+    # "jiazhuang": "https://cq.meituan.com/jiazhuang/pn{}/",
+    # "yiliao": "https://cq.meituan.com/yiliao/pn{}/",
+    # "jiaoyupeixun": "https://cq.meituan.com/xuexipeixun/pn{}/"
 }
 
 hotel_url = "https://ihotel.meituan.com/hbsearch/HotelSearch?utm_medium=pc&version_name=999.9&cateId=20&attr_28=129&uuid=C1EA418E59192BE72919EF4468CFA088AFC416E2D10120BB18440DA3BF854258%401562656719051&cityId=45&offset={}&limit=20&startDay=20190908&endDay=20190910&q=&sort=defaults&X-FOR-WITH=c0dGLE3siaNf6WANtDItcccQMpQvbJuHHXiSyb7dP6m2wrWAH9gtxqbHsOreGP5zCtGc3QfExeVZqpNhptxz%2F4GSYcf6XcJfaYhBt4PRoOvn4MvIuqvPMq1PsVCWsxXgDy3qFLJNM4b6nfIf6pX5og%3D%3D"
 hotel_detail = "https://hotel.meituan.com/{}/"
 session = create_meituan_session()
+session.headers["Host"] = "cq.meituan.com"
 driver = create_webdriver()
 
 
@@ -162,6 +190,7 @@ def parse_pages(url):
         except:
             print(traceback.print_exc())
     elif "jiehun" in url:
+        print("parse jiehun {}".format(url))
         parse_jiehun_item(session, url)
     else:
         r = session.get(url)
@@ -231,14 +260,14 @@ def total_pages(url):
                         temp.append(item.text)
         last_page = int(temp[-1])
     except Exception as e:
-        print(traceback.print_exc())
+        # print(traceback.print_exc())
         last_page = 1
     print(last_page)
     count = 1
     while count <= last_page:
         page_url = url.format(count)
         try:
-            time.sleep(random.uniform(0.5, 1))
+            time.sleep(random.uniform(1, 2))
             parse_pages(page_url)
         except KeyboardInterrupt:
             exit(0)
@@ -252,12 +281,14 @@ def get_hotelids(url):
     session.headers["Accept"] = "application/json, text/plain, */*"
     session.headers["Origin"] = "https://hotel.meituan.com"
     session.headers["Referer"] = "https://hotel.meituan.com/chongqing/"
+    session.headers["Host"] = "ihotel.meituan.com"
+    # session.headers["Cookie"] = "_lxsdk_cuid=16bd9b99c9ec8-035ab9a6954478-36664c08-1fa400-16bd9b99ca0c8; iuuid=8369B0074906E31235D094B1D10CB5398B04DC92AAFDBADB7477CB96EEFF986E; cityname=%E9%87%8D%E5%BA%86; _lxsdk=8369B0074906E31235D094B1D10CB5398B04DC92AAFDBADB7477CB96EEFF986E; _hc.v=10962146-cd2f-a7a9-c15a-d942a6e12989.1562744821; ci=45; rvct=45%2C1%2C114; _ga=GA1.2.1247011406.1563181057; _lx_utm=utm_source%3Dso.com%26utm_medium%3Dorganic; uuid=36ead8f00d76403086f7.1566196693.1.0.0; IJSESSIONID=jaox69mj94cq1rkw4jpfsxtuw; _lxsdk_s=16ca898f3b0-645-dd2-166%7C%7C39"
     temp = 0
     count = 0
     while True:
-        time.sleep(random.uniform(0.5, 2))
+        time.sleep(random.uniform(1, 2))
         r = session.get(url.format(temp))
-        print(url.format(temp))
+        # print(url.format(temp))
         items = r.json().get("ct_pois")
         temp = temp + 20
         if not items:
@@ -283,11 +314,15 @@ def get_hotel_detail(url):
     else:
         result = {}
         time.sleep(random.uniform(1, 3))
+        # session = create_meituan_session()
+        session.headers["Host"] = "hotel.meituan.com"
         r = session.get(url)
         print("parse hotel {}".format(url))
         r.encoding = "utf-8"
         soup = BeautifulSoup(r.text, 'lxml')
         naspan = soup.find("div", {"class": "breadcrumb-nav"})
+        if not naspan:
+            naspan = soup.find_all("span", class_="fs26 fc3 pull-left bold")
         result["shop"] = naspan.text.strip()
         result["url"] = url
         result["openTime"] = "全天"
@@ -321,6 +356,7 @@ def get_hotel_detail(url):
             print("获取值为空 {}".format(url))
 
 
+# @second_run
 def start():
     for item in url_map:
         try:
@@ -333,7 +369,7 @@ def start():
                     if a.get("href") and a.get("href").startswith("//"):
                         url = "https:" + a.get("href") + "pn{}/"
                         try:
-                            total_pages(url)
+                            sa = total_pages(url)
                         except Exception as e:
                             print(traceback.print_exc())
             elif "meishi" in url_map[item]:
@@ -348,7 +384,7 @@ def start():
                             #     STATUS = True
                             # if not STATUS:
                             #     continue
-                            total_pages(url)
+                            sa = total_pages(url)
                         except Exception as e:
                             print(traceback.print_exc())
             else:
@@ -358,7 +394,10 @@ def start():
                     try:
                         div = soup.find_all("div", class_="filter-component")
                         if not div:
-                            raise Exception("没有找到 {}".format(url_map[item].format(1)))
+                            if "没有符合条件的商家" in r.text:
+                                continue
+                            else:
+                                raise Exception("没有找到 {}".format(url_map[item].format(1)))
                         div = div[-1]
                         div = div.find_all("div", class_="tag-group tag-group-expend")
                         if not div:
@@ -372,7 +411,7 @@ def start():
                                     d_u = "https:" + a.get("href") + "pn{}/"
                                     # ares = a.text.strip()
                                     try:
-                                        total_pages(d_u)
+                                        sa = total_pages(d_u)
                                     except Exception as e:
                                         print(e)
                     except Exception as e:
@@ -383,7 +422,7 @@ def start():
                         if ite.get("href") and ite.get("href").startswith("//"):
                             url = "https:" + ite.get("href") + "pn{}/"
                             try:
-                                total_pages(url)
+                                sa = total_pages(url)
                             except Exception as e:
                                 print(e)
         except:
